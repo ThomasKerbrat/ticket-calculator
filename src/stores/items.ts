ffa5fdf67e59d383d88d33ae6677943a9e878343
Thomas Kerbrat <kerbrat@outlook.com>
2025-10-26 23:04:49 +0100

    persists ticket items in localStorage

--- src/stores/items.ts
+++ src/stores/items.ts
@@ -3,8 +3,31 @@ import { defineStore } from "pinia";
 import { TicketItem, type TicketItemDraft } from "@/models/TicketItem";
 
 export const useItemsStore = defineStore("items", () => {
-    let nextId = 1;
-    const items = ref<TicketItem[]>([]);
+    const items = ref<TicketItem[]>(_loadFromLocalStorage());
+    let nextId = items.value.length > 0 ? Math.max(...items.value.map((item: any) => item.id)) + 1 : 1;
+
+    function _loadFromLocalStorage(): TicketItem[] {
+        const items: TicketItem[] = [];
+
+        const str = localStorage.getItem("ticket-items");
+        let json = null;
+
+        if (str != null) {
+            try {
+                json = JSON.parse(str);
+
+                for (let item of json) {
+                    items.push(new TicketItem(item.id, item.label, item.price, item.quantity));
+                }
+            } catch { }
+        }
+
+        return items;
+    }
+
+    function _saveToLocalStorage(): void {
+        localStorage.setItem("ticket-items", JSON.stringify(items.value));
+    }
 
     function getItemById(id: number): TicketItem | undefined {
         for (let item of items.value) {
@@ -14,27 +37,31 @@ export const useItemsStore = defineStore("items", () => {
         }
     }
 
-    function addItem(draft: TicketItemDraft) {
+    function addItem(draft: TicketItemDraft): void {
         if (draft.label && draft.price && draft.quantity) {
             items.value.push(new TicketItem(nextId, draft.label, draft.price, draft.quantity));
+            _saveToLocalStorage()
             nextId++;
         }
     }
 
-    function editItem(id: number, draft: TicketItemDraft) {
+    function editItem(id: number, draft: TicketItemDraft): void {
         const item = getItemById(id);
         if (!item) { return; }
 
         if (draft.label) { item.label = draft.label; }
         if (draft.price) { item.price = draft.price; }
         if (draft.quantity) { item.quantity = draft.quantity; }
+
+        _saveToLocalStorage()
     }
 
-    function removeItem(id: number) {
+    function removeItem(id: number): void {
         const index = items.value.findIndex(item => item.id === id);
 
         if (index != undefined) {
             items.value.splice(index, 1);
+            _saveToLocalStorage()
         }
     }
 
